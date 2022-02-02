e8773a7ff6dae9e4eb3a84a7a9b78b2980b2c322
Phong-nn <phong_nguyenn@hotmail.com>
2022-02-02 15:46:12 +1100

    Functionality working. Still requires cleaning/optimisation

--- Trivia Questionnaire/src/App.js
+++ Trivia Questionnaire/src/App.js
@@ -6,30 +6,65 @@ import Questions from "./Components/Questions"
 
 export default function App() {
   const [start, setStart] = React.useState(false)
+  const [checked, setChecked] = React.useState(false)
+  
 
   const [allQuestions, setAllQuestions] = React.useState([])
+  const [selected, setSelected] = React.useState([])
     
     
+// try make state object array > map allquestions into new array, getting id and having state selected, put it in prop
+
   React.useEffect(() => {
       fetch("https://opentdb.com/api.php?amount=10")
           .then(res => res.json())
           .then(data => setAllQuestions(data.results))
+
   }, [])
 
 
   function checkAnswers() {
+    setChecked(true)
+  }
 
+
+
+  function initialiseStated() {
+    for (let i = 1; i <= 10; i++) {
+      let temp = {
+        id: i,
+        select: ""
+      }
+      setSelected(prev => [temp, ...prev])
+    }
+    
+  }
+
+
+  function updateSelected(id, text) {
+    setSelected(prev => prev.map(data => (
+      data.id === id ? { ...data, select:text } : data)
+    ))
   }
 
   function startQuiz() {
     setStart(prev => !prev)
+    initialiseStated()
+    console.log(selected.length)
+    
   }
 
+  let id = 0
   const quiz = allQuestions.map(question => (
       <Questions 
-        key={nanoid()}
         {...question}
+        key={nanoid()}
+        id={++id}
+        checked={checked}
+        selected={selected}
+        updateSelected={updateSelected}
       />
+      
   ))
 
   return (
--- Trivia Questionnaire/src/Components/Questions.js
+++ Trivia Questionnaire/src/Components/Questions.js
@@ -1,39 +1,67 @@
 import React from "react"
 import he from "he"
+import {nanoid} from "nanoid"
 
 export default function Questions(props) {
+    const [questions, setQuestions] = React.useState([])
 
-    // function handleChange(event) {
-    //     const {name, value} = event.target
-    //     setMeme(prevMeme => ({
-    //         ...prevMeme,
-    //         [name]: value
-    //     }))
-    // }
+
     
     let answers = []
-    if (props.type !== "boolean") {
-
-        answers.push(he.decode(props.correct_answer))
-        for (let i=0; i < props.incorrect_answers.length; i++) {
-            answers.push(he.decode(props.incorrect_answers[i]))
-        }
-
-        //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array#:~:text=209-,You,-can%20do%20it
-        answers = answers
-            .map(value => ({ value, sort: Math.random() }))
-            .sort((a, b) => a.sort - b.sort)
-            .map(({ value }) => value)
-            
-    } else {
-        answers = ["True", "False"]
+    answers.push(he.decode(props.correct_answer))
+    for (let i=0; i < props.incorrect_answers.length; i++) {
+        answers.push(he.decode(props.incorrect_answers[i]))
     }
+
+    function randomise(arr) {
+        //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array#:~:text=209-,You,-can%20do%20it
+        if (props.type !== "boolean") {
+            arr = arr
+                .map(value => ({ value, sort: Math.random() }))
+                .sort((a, b) => a.sort - b.sort)
+                .map(({ value }) => value)
+        } else {
+            arr = ["True", "False"]
+        }
+        
+
+        arr =   arr.map(ans => (
+            <button key={nanoid()} onClick={updateAnswer}>{ans}</button>
+        ))
+        setQuestions(arr)
+    }
+    
+    React.useEffect(() => {
+        randomise(answers)
+    }, [])
+
+    React.useEffect(() => {
+        if (props.checked) {
+            if (props.selected.find(data => data.id === props.id).select === props.correct_answer) {
+                console.log("CORRECT")
+            } else {
+                console.log("WRONG")
+            }
+        }
+        
+    }, [props.checked])
+
+
+
+    function updateAnswer(event) {
+        props.updateSelected(props.id, event.target.innerText)
+    }
+
     
     return (
         <main>
             <h1>{he.decode(props.question)}</h1>
-            <h3>{answers}</h3>
-            <h4>{props.correct_answer}</h4>
+            {questions}
+            <h4>Correct answers: {props.correct_answer}</h4>
+            <h4>Current: {props.id}</h4>
+
+
+            <hr />
         </main>
     )
 }
