2c6cabb48426dbe6681790b989549beefa8de725
PHO-NG <phong_nguyenn@hotmail.com>
2023-08-03 13:21:26 +1000

    fix friend request

--- chat/src/app/api/friends/add/route.ts
+++ chat/src/app/api/friends/add/route.ts
@@ -1,13 +1,17 @@
 import { fetchRedis } from "@/helpers/redis"
 import { authOptions } from "@/lib/auth"
+import { db } from "@/lib/db"
 import { addFriendValidator } from "@/lib/validations/add-friend"
 import { getServerSession } from "next-auth"
+import {z} from "zod"
 
 export async function POST(req: Request) {
     try {
-        const body = await req.json()
+        const body = await req.json() as {email: string | null}
 
         const {email: emailToAdd} = addFriendValidator.parse(body.email)
+
+        const idToAdd = await fetchRedis('get', `user:email:${emailToAdd}`) as string
 
         const RESTResponse = await fetch(`${process.env.UPSTASH_REDIS_REST_URL}/get/user:email${emailToAdd}`, {
             headers: {
@@ -18,7 +22,7 @@ export async function POST(req: Request) {
 
         const data = await RESTResponse.json() as {result: string | null}
         const session = await getServerSession(authOptions)
-        const idToAdd = data.result
+        // const idToAdd = data.result
         if (!idToAdd) {
             return new Response('This person does not exist.', {status: 400})
         }
@@ -37,8 +41,21 @@ export async function POST(req: Request) {
             return new Response('Already added this user', { status: 400})
         }
         
+        const isAlreadyFriends = (await fetchRedis('sismember', `user:${session.user.id}:friends`, idToAdd)) as 0 | 1
+
+        if (isAlreadyFriends) {
+            return new Response('Already friends with this user', { status: 400})
+        }
+
+        db.sadd(`userL:${idToAdd}:incoming_friend_requests`, session.user.id)
+        return new Response('OK')
+        
         
     } catch (error) {
-        
+        if (error instanceof z.ZodError) {
+            return new Response("Invalid request payload", { status: 422})
+        }
+
+        return new Response("Ivalid request error", { status: 400})
     }
 }
