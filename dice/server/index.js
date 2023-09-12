07181c7a106f68223408784a18ca9e2398835a2f
PHO-NG <phong_nguyenn@hotmail.com>
2023-09-12 14:47:17 +1000

    change server to js from ts for deployment

--- dice/server/index.js
+++ dice/server/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const http = require('http')
-const app = express()
+const cors = require('cors')
 const socketio = require('socket.io')
 
-const router = require('./router');
-
-const server = http.createServer(app)
+const router = require('./router')
 const PORT = process.env.PORT || 3001
+
+const app = express()
+const server = http.createServer(app)
 const io = socketio(server)
 
 app.use(cors());
